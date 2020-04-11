54e2cc9477595d4b3c871a383046d40a02125bbd
Nicolas Martignoni <nicolas@martignoni.net>
2020-04-11 12:32:36 +0200

    Purge CSS via new resources.PostProcess feature

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [ './hugo_stats.json' ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+});
+
 module.exports = {
-  plugins: {
-    '@fullhuman/postcss-purgecss': {
-      content: [
-        './**/*.html',
-        './**/*.css'
-      ],
-    },
-    autoprefixer: {},
-    cssnano: {preset: 'default'}
-  }
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        purgecss
+    ]
 };
