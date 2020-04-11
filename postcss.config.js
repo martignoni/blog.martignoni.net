9753ea1c5f8829843b50a3c4f3b8f6d208ebbe54
Nicolas Martignoni <nicolas@martignoni.net>
2020-04-11 14:50:26 +0200

    Remove for good buggy post-processing

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer'),
-        purgecss
+        require('autoprefixer')
+//         purgecss
     ]
 };
