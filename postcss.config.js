3e4058834c7fe028780cd1cd7b6b4015d0d427f5
Nicolas Martignoni <nicolas@martignoni.net>
2026-02-20 14:38:10 +0100

    Simply CSS post-processing

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,21 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ './hugo_stats.json' ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
+const purgeCSSPlugin = require('@fullhuman/postcss-purgecss');
+
+const purgecss = purgeCSSPlugin({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: content => {
+    const els = JSON.parse(content).htmlElements;
+    return [
+      ...(els.tags || []),
+      ...(els.classes || []),
+      ...(els.ids || []),
+    ];
+  },
+  // https://purgecss.com/safelisting.html
+  safelist: []
 });
 
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
-//         purgecss
-    ]
+  plugins: [
+    process.env.HUGO_ENVIRONMENT !== 'development' ? purgecss : null,
+  ]
 };
